be83bc7acd6fa981e50b7f2649617c6eba130ca7
seshu babu karanam <108258542+seshubabukaranam@users.noreply.github.com>
2022-10-19 00:38:09 +0530

    feat
    Added new features-language translator.

--- apps.js
+++ apps.js
@@ -20,17 +20,28 @@ function errorHandler(error) {
 }
 
 
-function clickHandler() {
-    var inputText = txtInput.value; // taking input
+function clickHandler() 
+{
+    
 
-    // calling server for processing
-    fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; // output
-           })
-        .catch(errorHandler)
-};
+    if(txtInput.value <= 0)
+     {
+       alert("Please enter text");
+     }
+    else
+    {
+        var inputText = txtInput.value; // taking input
+
+        // calling server for processing
+        fetch(getTranslationURL(inputText))
+            .then(response => response.json())
+            .then(json => {
+                var translatedText = json.contents.translated;
+                outputDiv.innerText = translatedText; // output
+               })
+            .catch(errorHandler)
+    }
+
+    };
 
 btnTranslate.addEventListener("click", clickHandler)
